f3ccb66c1cde11f55f2ffba6ad03e082f3ea0d4f
sukhrobbeksoatov <suhrobbek_soatov@mail.ru>
2023-02-23 21:51:55 +0500

    add changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
+// elements
+const elDestImg = document.querySelector("[data-dest-img]");
+const elDestHeading = document.querySelector("[data-dest-heading]");
+const elDestDescription = document.querySelector("[data-dest-desc]");
+const elDestDistance = document.querySelector("[data-dest-distance]");
+const elDestTime = document.querySelector("[data-dest-time]");
+
+// event delegation
 document.addEventListener("click", (evt) => {
   siteNavToggleClick(evt);
+  destinationItemClick(evt);
 });
 
 // Sitenav toggle click
@@ -9,4 +18,21 @@ function siteNavToggleClick(evt) {
   if (!el) return;
 
   el.classList.toggle("opened");
+}
+
+function destinationItemClick(evt) {
+  const el = evt.target.closest("[data-dest-item]");
+
+  if (!el) return;
+
+  data.destinations.forEach((planet) => {
+    if (el.innerText.toLowerCase() === planet.name.toLowerCase()) {
+      elDestImg.src = planet.images.png;
+      elDestImg.srcset = `${planet.images.png} 1x, ${planet.images.png2x} 2x`;
+      elDestHeading.textContent = planet.name;
+      elDestDescription.textContent = planet.description;
+      elDestDistance.textContent = planet.distance;
+      elDestTime.textContent = planet.travel;
+    }
+  });
 }
