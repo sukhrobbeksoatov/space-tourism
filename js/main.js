b942b7b2c4259ecad6dd4c9db2d438b26c3e1a54
sukhrobbeksoatov <suhrobbek_soatov@mail.ru>
2023-02-24 01:59:34 +0500

    add changes

--- js/main.js
+++ js/main.js
@@ -113,7 +113,23 @@ function technologyNavListItemClick(evt) {
   });
 }
 
+// Resize event
 window.addEventListener("resize", () => {
+  data.technology.forEach((technology) => {
+    if (elTechnologyImg.dataset.launchImg === technology.name) {
+      if (window.innerWidth < 992) {
+        elTechnologyImg.src = technology.images.landscape;
+        elTechnologyImg.srcset = `${technology.images.landscape} 1x, ${technology.images.landscape2x} 2x`;
+      } else {
+        elTechnologyImg.src = technology.images.portrait;
+        elTechnologyImg.srcset = `${technology.images.portrait} 1x, ${technology.images.portrait2x} 2x`;
+      }
+    }
+  });
+});
+
+// Load event
+window.addEventListener("load", () => {
   data.technology.forEach((technology) => {
     if (elTechnologyImg.dataset.launchImg === technology.name) {
       if (window.innerWidth < 992) {
