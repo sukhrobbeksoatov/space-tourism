233c755e13c566e74227effb79846ef1bf3e076f
sukhrobbeksoatov <suhrobbek_soatov@mail.ru>
2023-02-24 02:06:22 +0500

    add changes

--- js/main.js
+++ js/main.js
@@ -16,6 +16,38 @@ const elCrewImg = document.querySelector("[data-crew-img]");
 // Technology
 const elTechnologyImg = document.querySelector(".launch-technology__img");
 
+// Load event
+window.addEventListener("load", () => {
+  data.technology.forEach((technology) => {
+    if (elTechnologyImg.dataset.launchImg === technology.name) {
+      if (window.innerWidth < 992) {
+        elTechnologyImg.src = technology.images.landscape;
+        elTechnologyImg.srcset = `${technology.images.landscape} 1x, ${technology.images.landscape2x} 2x`;
+        return;
+      } else {
+        elTechnologyImg.src = technology.images.portrait;
+        elTechnologyImg.srcset = `${technology.images.portrait} 1x, ${technology.images.portrait2x} 2x`;
+        return;
+      }
+    }
+  });
+});
+
+// Resize event
+window.addEventListener("resize", () => {
+  data.technology.forEach((technology) => {
+    if (elTechnologyImg.dataset.launchImg === technology.name) {
+      if (window.innerWidth < 992) {
+        elTechnologyImg.src = technology.images.landscape;
+        elTechnologyImg.srcset = `${technology.images.landscape} 1x, ${technology.images.landscape2x} 2x`;
+      } else {
+        elTechnologyImg.src = technology.images.portrait;
+        elTechnologyImg.srcset = `${technology.images.portrait} 1x, ${technology.images.portrait2x} 2x`;
+      }
+    }
+  });
+});
+
 // event delegation
 document.addEventListener("click", (evt) => {
   siteNavToggleClick(evt);
@@ -112,33 +144,3 @@ function technologyNavListItemClick(evt) {
     }
   });
 }
-
-// Resize event
-window.addEventListener("resize", () => {
-  data.technology.forEach((technology) => {
-    if (elTechnologyImg.dataset.launchImg === technology.name) {
-      if (window.innerWidth < 992) {
-        elTechnologyImg.src = technology.images.landscape;
-        elTechnologyImg.srcset = `${technology.images.landscape} 1x, ${technology.images.landscape2x} 2x`;
-      } else {
-        elTechnologyImg.src = technology.images.portrait;
-        elTechnologyImg.srcset = `${technology.images.portrait} 1x, ${technology.images.portrait2x} 2x`;
-      }
-    }
-  });
-});
-
-// Load event
-window.addEventListener("load", () => {
-  data.technology.forEach((technology) => {
-    if (elTechnologyImg.dataset.launchImg === technology.name) {
-      if (window.innerWidth < 992) {
-        elTechnologyImg.src = technology.images.landscape;
-        elTechnologyImg.srcset = `${technology.images.landscape} 1x, ${technology.images.landscape2x} 2x`;
-      } else {
-        elTechnologyImg.src = technology.images.portrait;
-        elTechnologyImg.srcset = `${technology.images.portrait} 1x, ${technology.images.portrait2x} 2x`;
-      }
-    }
-  });
-});
